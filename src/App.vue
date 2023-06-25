18f5a1276ed2e7c41a70264e771fddb08e9bf7a4
viola <1392546252@qq.com>
2023-06-25 17:03:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+    const modalConfirm = () => {
+      console.log('modal confirm');
+    }
+
+    const modalClose = () => {
+      console.log('modal close');
+    }
+</script>
+
+<template>
+  <Modal
+    :show="true"
+    :width="350"
+    :borderRadius="10"
+    headerColor="'#333'"
+    headerTextColor="#fff"
+    headerText="Modal Header"
+    contentText="lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    contentTextColor="#333"
+    position="center"
+    :buttonGroupShow="true"
+    confirmText="是"
+    cancelText="否"
+    @confirm="modalConfirm"
+    @close="modalClose"
+  />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,79 @@
+<template>
+
+</template>
+
+<script setup>
+    
+    // 用diefineProps定义props
+    defineProps({
+        show: {
+            type: Boolean,
+            default: false,
+        },
+        width: {
+            type: Number,
+            default: 300,
+        },
+        borderRadius: {
+            type: Number,
+            default: 0,
+        },
+        headerColor: {
+            type: String,
+            default: '#000',
+        },
+        headerTextColor: {
+            type: String,
+            default: '#fff',
+        },
+        headerText: {
+            type: String,
+            default: 'modal',
+        },
+        contentText: {
+            type: String,
+            default: '',
+        },
+        contentTextColor: {
+            type: String,
+            default: '#000',
+        },
+        position: {
+            type: String,
+            default: 'top',
+        },
+        buttonGroupShow: {
+            type: Boolean,
+            default: false,
+        },
+        confirmText: {
+            type: String,
+            default: 'confirm',
+        },
+        cancelText: {
+            type: String,
+            default: 'cancel',
+        },
+    })
+
+    // 用defineEmits定义事件
+    const emits = defineEmits(['confirm', 'close'])
+
+    const modalConfirm = () => {
+        console.log('modal confirm');
+        // 触发事件
+        emit('confirm')
+    }
+
+    const modalClose = () => {
+        console.log('modal close');
+        // 触发事件
+        emit('close')
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
